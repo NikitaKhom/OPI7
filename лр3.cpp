606c5f2558c161926ef38a00e3965c67cfb83a90
NikitaKhom <homutovnikita1@gmail.com>
2023-11-15 12:50:16 +0200

    created

---
+++ лр3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "windows.h"
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int num, a, b;
+	cout << "\nВведіть num:";
+	cin >> num;
+	a = num % 10;
+	b = (num / 10) % 10;
+	cout << "Result :" << (a < b);
+}
