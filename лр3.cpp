760f84528297f7c19858b26427ef9bef591519ce
NikitaKhom <homutovnikita1@gmail.com>
2023-11-21 14:40:30 +0200

    printf scanf_s

--- лр3.cpp
+++ лр3.cpp
@@ -2,22 +2,23 @@
 #include <iomanip>
 #include "math.h"
 #include "windows.h"
+#include "studio.h"
 
 
 using namespace std;
 int main()
 {
     SetConsoleOutputCP(1251);
-    int a, b;
+    int a, c;
     double y, x, h;
-    cout << "Введіть значення a =";
-    cin >> a;
-    cout << "Введіть значення b =";
-    cin >> b;
-    cout << "Введіть значення h =";
-    cin >> h;
+    printf("Введіть значення a:");
+    scanf_s("%d", &a) << '\n';
+    printf("Введіть значення c:")
+    scanf_s("%d", &c) << '\n';
+    printf("Введіть значення h:");
+    scanf_s("%d", &h) << '\n';
     x = a;
-    while (x <= b)
+    while (x <= c)
     {
         y = sin(fabs((x)) + pow(cos(x), 2));
         cout << setw(5) << x << "\t|\t" << setw(10) << y << endl;
